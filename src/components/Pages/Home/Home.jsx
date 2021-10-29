1d87f383288a82604ab5619bbe7a72f50c841bfe
coderander <frokorie013@gmail.com>
2021-10-29 21:11:15 +0100

    updated landing page text

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -10,20 +10,25 @@ export default function Home() {
       <div className="container">
         <div className="img-div">
           <img src={homeImage} alt="home logo" className="home-image" />
-         
-            <Typography className="details details-title" variant="h5">
-              ELDER
-            </Typography>
-            <Typography className="details details-name" variant="h4">
-              ISAAC AKPOWENE
-              <br /> EGBI, (JP)
-            </Typography>
+
+          <Typography className="details details-title" variant="h5">
+            ELDER
+          </Typography>
+          <Typography className="details details-name" variant="h4">
+            ISAAC AKPOWENE
+            <br /> EGBI, (JP)
+          </Typography>
         </div>
         <div className="caption">
-          <Typography variant="h6" className="caption-text">
-            Austin wake up and give this capion now that we still have time....
-          </Typography>
-          <NavLink to="/bio" className="more-button">More</NavLink>
+          <h3 className="caption-text">
+            We live for the purpose of humanity and benovolence.....
+          </h3>
+          <h3 className="caption-text">
+            <span>adding value to mankind.</span>
+          </h3>
+          <NavLink to="/bio" className="more-button">
+            More
+          </NavLink>
         </div>
       </div>
     </div>
